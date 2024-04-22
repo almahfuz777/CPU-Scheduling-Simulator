ab676e35051e99ef5be515e83e83466a5e694ed2
Abdullah Al Mahfuz <almahfuz777@gmail.com>
2024-04-23 04:17:21 +0600

    landing page

--- app/src/main/java/com/example/cpu_scheduling_simulator/Process.java
+++ app/src/main/java/com/example/cpu_scheduling_simulator/Process.java
@@ -1,5 +1,4 @@
 package com.example.cpu_scheduling_simulator;
-
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,30 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
+public class Process extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.process);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.process_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button button = (Button) findViewById(R.id.getStarted);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Process.class);
-                startActivity(intent);
-            }
         });
     }
 }
